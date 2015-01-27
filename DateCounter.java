b2180876010dcefb0c1dc67bb2cdb72d69ccfcc6
cfmihm <cfmihm@users.noreply.github.com>
2015-01-27 11:52:05 -0800

    Initial commit with stubs and set up methods

---
+++ DateCounter.java
@@ -0,0 +1,32 @@
+public class DateCounter {
+
+	public static void main(String[] args) {
+	
+	}
+	
+	public static boolean isLeapYear(int year) {
+		return false;
+	}
+	
+	public static int daysInMonth(int year, int month) {
+		return 0;
+	}
+	
+	public static boolean isValidDate(int year, int month, int day) {
+		return false;
+	}
+	
+	public static int daysBetween(int year0, int month0, int day0, int year1, int month1, int day1) {
+		return 0;
+	}
+	
+	public static boolean hasLeapSecond(int year) {
+		return false;
+	}
+	
+	public static boolean hasLeapSecond(int year, int month, int day) {
+		return false;
+	}
+
+
+}
