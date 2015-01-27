fc30214ecc7c7ee3411df79af33a8a7ed31ee6a4
cfmihm <cfmihm@users.noreply.github.com>
2015-01-27 11:17:33 -0800

    Fixed, now catches negatives and cleaned up formats and semicolons

--- ChangeMaker.java
+++ ChangeMaker.java
@@ -4,13 +4,12 @@ public class ChangeMaker {
 		int cents = 0;
 
 		try {
-		
-			if (cents < 0) {
-				throw new IllegalArgumentException();
-			};
-		
 			cents = Integer.parseInt(args[0]); // converts string to int
 			
+			if (cents < 0) {
+				throw new IllegalArgumentException("negative value");
+			}
+
 			int[] answer = getChange(cents);
 			
 			System.out.println("Quarters: " + answer[0]);
@@ -25,32 +24,32 @@ public class ChangeMaker {
 			System.out.println("Supplied value is not an integer.");
 		} catch(IllegalArgumentException e) {
 			System.out.println("Cannot make change for negative cents.");
-		};
-	};
+		}
+	}
 	
 	public static int getQuarters(int cents) {
 		int quarterValue = 25;
 		int quarters = (cents / quarterValue);	
 		return quarters;
-	};
+	}
 	
 	public static int getDimes(int cents) {
 		int dimeValue = 10;
 		int dimes = (cents / dimeValue);	
 		return dimes;
-	};
+	}
 	
 	public static int getNickels(int cents) {
 		int nickelValue = 5;
 		int nickels = (cents / nickelValue);	
 		return nickels;
-	};
+	}
 	
 	public static int getPennies(int cents) {
 		int pennyValue = 1;
 		int pennies = (cents / pennyValue);	
 		return pennies;
-	};
+	}
 	
 	public static int[] getChange(int cents) {
 		int totalQuarters = 0;
@@ -63,27 +62,26 @@ public class ChangeMaker {
 		if (remainingCents >= 25) {
 			totalQuarters = getQuarters(cents);
 			remainingCents = cents - (totalQuarters * 25);
-		};
+		}
 		
 		if (remainingCents >= 10) {
 			totalDimes = getDimes(remainingCents);
 			remainingCents = remainingCents - (totalDimes * 10);		
-		};
+		}
 			
 		if (remainingCents >= 5) {
 			totalNickels = getNickels(remainingCents);
 			remainingCents = remainingCents - (totalNickels * 5);
-		};
+		}
 			
 		if (remainingCents >= 1) {
 			totalPennies = getPennies(remainingCents);	
 			remainingCents = remainingCents - (totalPennies * 1);
-		};
+		}
 		
 		int[] change = {totalQuarters, totalDimes, totalNickels, totalPennies};
 		
 		return change;
-	};
-		
+	}	
 
-};
+}
